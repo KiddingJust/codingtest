e620ef0790fbaa2ad68e5ab04aac30d753a51022
Kidding Just <rlarkdlr@gmail.com>
2019-11-16 14:03:57 +0900

    queue

--- src/org/kidding/test/stackandqueue/PrinterQueue.java
+++ src/org/kidding/test/stackandqueue/PrinterQueue.java
@@ -5,9 +5,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.Queue;
 
 public class PrinterQueue {
 
